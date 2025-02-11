f0c82a611d4e15578c7f5605c4e429d011eb73f5
bradygess <bradygess13@gmail.com>
2025-02-11 18:40:41 -0500

    Commit

---
+++ spotify_audio.py
@@ -0,0 +1,13 @@
+
+def pause_toggle(frame):
+    #index, confidence = predict(frame)
+    #pause
+    return 0
+
+def play_music(frame):
+    # index, confidence = predict(frame)
+    return 0
+
+def run_audio(frame):
+    pause_toggle(frame)
+    play_music(frame)
