b5da07ea1672bdbe7a2955cbbfff3da4b0d041fd
bradygess <bradygess13@gmail.com>
2025-02-11 18:30:29 -0500

    Commit

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 import cv2 as cv
-import time
-# git push origin main
-
-def safe_capture():
-    while True:
-        try:
-            cap = cv.VideoCapture(0)
-            if cap.isOpened():
-                return cap
-            else:
-                print("Could not open video capture device. Retrying...")
-                time.sleep(1)  # Wait for a second before retrying
-        except Exception as e:
-            print(f"Error while creating video capture: {e}")
-            time.sleep(1)  # Wait for a second before retrying
-
 
 def main():
-    cap = safe_capture()
+    cap = cv.VideoCapture(0)
     while cap.isOpened():
         ret, frame = cap.read()
         print(frame)
 
 main()
+
